a4fb743e7a42996467fa0a81e58b27ef708e877b
Игорь Колядин <Digital-Corsair@yandex.com>
2021-10-11 23:24:50 +0400

    Добавьте файлы проекта.

---
+++ AandSD_lab2/AandSD_lab2.cpp
@@ -0,0 +1,680 @@
+﻿#include "Set.h"
+#include <iostream>
+#include <conio.h>
+#include <string>
+using namespace std;
+
+int CheckNumber() {
+	char Data[32];
+	char Mask[12] = { '1','2','3','4','5','6','7','8','9','0', '-' };
+	int Checker = 0;
+	int Count = 0;
+	std::cin >> Data;
+	for (int i = 0; i < strlen(Data); i++)
+	{
+		if (Data[i] == '-') Count++;
+		for (int j = 0; j < 12; j++)
+		{
+			if (Data[i] == Mask[j]) Checker++;
+		}
+	}
+	if ((Checker != strlen(Data)) || (Checker == 0) || (Count > 1)) throw "[!]: Incorrect data format...\n";
+	int Number = (int)atof(Data);
+	return Number;
+}
+
+double CheckNumberDouble() {
+	char Data[32];
+	char Mask[12] = { '1','2','3','4','5','6','7','8','9','0', '-', '.' };
+	int Checker = 0;
+	int Count = 0;
+	std::cin >> Data;
+	for (int i = 0; i < strlen(Data); i++)
+	{
+		if (Data[i] == '-') Count++;
+		for (int j = 0; j < 12; j++)
+		{
+			if (Data[i] == Mask[j]) Checker++;
+		}
+	}
+	if ((Checker != strlen(Data)) || (Checker == 0) || (Count > 1)) throw "[!]: Incorrect data format...\n";
+	double Number = (double)atof(Data);
+	return Number;
+}
+
+int GetKey()
+{
+	int Key = _getch();
+	if ((Key == 0) || (Key == 224)) Key = _getch();
+	return Key;
+}
+
+void MenuInt(Set<int> &rhs);
+void MenuDouble(Set<double> &rhs);
+void MenuString(Set<std::string> &rhs);
+
+
+int main()
+{
+	Set<int> A;
+	Set<double> B;
+	Set<std::string> C;
+
+	int Repeat = 1;
+	std::cout << "Hello, User ^_^!\n\n";
+	std::cout << "[0]: Set of int\n[1]: Set of double\n[2]: Set of string\n"<< std::endl;
+	while (Repeat != 0)
+	{
+		int k = GetKey();
+		switch (k)
+		{
+		case 48:
+			system("cls");
+			MenuInt(A);
+			break;
+		case 49:
+			system("cls");
+			MenuDouble(B);
+			break;
+		case 50:
+			system("cls");
+			MenuString(C);
+			break;
+		default: std::cout << "Select [0-2]" << endl;
+		}
+	}
+}
+
+
+void MenuWorkSetsString(Set<std::string>&A) {
+	int Repeat = 1;
+	std::cout << A << std::endl;
+	std::cout << "[0]: Sum of sets\n[1]: Difference of sets\n[2]: Union of sets\n[3]: Back\n" << std::endl;
+	while (Repeat != 0)
+	{
+		int k = GetKey();
+		switch (k)
+		{
+		case 48:
+		{
+			system("cls");
+			try
+			{
+				std::cout << "Ented a size: ";
+				int Size = CheckNumber();
+				if (Size < 0) Size *= -1;
+
+				std::string* NewSet = new std::string[Size];
+				std::cin.ignore();
+				for (int i = 0; i < Size; i++) {
+					std::cout << "[" << i << "]: ";
+					std::getline(std::cin, NewSet[i]);
+				}
+				Set<std::string> B(Size, NewSet);
+				A = A + B;
+			}
+			catch (const char* err)
+			{
+				std::cerr << err;
+			}
+			MenuWorkSetsString(A);
+			break;
+		}
+		case 49:
+		{
+			system("cls");
+			try
+			{
+				std::cout << "Ented a size: ";
+				int Size = CheckNumber();
+				if (Size < 0) Size *= -1;
+
+				std::string* NewSet = new std::string[Size];
+				std::cin.ignore();
+				for (int i = 0; i < Size; i++) {
+					std::cout << "[" << i << "]: ";
+					std::getline(std::cin, NewSet[i]);
+				}
+
+				Set<std::string> B(Size, NewSet);
+				A = A - B;
+			}
+			catch (const char* err)
+			{
+				std::cerr << err;
+			}
+			MenuWorkSetsString(A);
+			break;
+		}
+		case 50:
+		{
+			system("cls");
+			try
+			{
+				std::cout << "Ented a size: ";
+				int Size = CheckNumber();
+				if (Size < 0) Size *= -1;
+				std::string* NewSet = new std::string[Size];
+				std::cin.ignore();
+				for (int i = 0; i < Size; i++) {
+					std::cout << "[" << i << "]: ";
+					std::getline(std::cin, NewSet[i]);
+				}
+				Set<std::string> B(Size, NewSet);
+				A = IntersectionSets(A, B);
+			}
+			catch (const char* err)
+			{
+				std::cerr << err;
+			}
+			MenuWorkSetsString(A);
+			break;
+		}
+		case 51:
+			system("cls");
+			MenuString(A);
+			break;
+		default: std::cout << "Select [0-3]" << std::endl;
+		}
+	}
+}
+
+
+void MenuSetString(Set<std::string>&A) {
+	int Repeat = 1;
+	std::cout << A << std::endl;
+	std::cout << "[0]: Add an element to a set\n[1]: Remove an element from a set\n[2]: Back\n" << std::endl;
+	while (Repeat != 0)
+	{
+		int k = GetKey();
+		switch (k)
+		{
+		case 48:
+		{
+			system("cls");
+			try
+			{
+				std::cout << "Ented an element: ";
+				std::string str;
+				std::getline(std::cin, str);
+				A = A + str;
+			}
+			catch (const char* err)
+			{
+				std::cerr << err;
+			}
+			MenuSetString(A);
+			break;
+		}
+		case 49:
+			system("cls");
+			try
+			{
+				std::cout << "Ented an element: ";
+				std::string str;
+				std::getline(std::cin, str);
+				A = A - str;
+			}
+			catch (const char* err)
+			{
+				std::cerr << err;
+			}
+			MenuSetString(A);
+			break;
+		case 50:
+			system("cls");
+			MenuString(A);
+			break;
+		default: std::cout << "Select [0-3]" << std::endl;
+		}
+	}
+}
+
+void MenuString(Set<std::string>&A) {
+	int Repeat = 1;
+	
+	std::cout << A << std::endl;
+	std::cout << "[0]: Create a new set\n[1]: Edit a set\n[2]: Operations with sets\n[3]: Clear Set\n[4]: Exit the program\n" << std::endl;
+	while (Repeat != 0)
+	{
+		int k = GetKey();
+		switch (k)
+		{
+		case 48:
+			system("cls");
+			{
+				A.~Set();
+				try {
+					std::cout << "Enter a size: ";
+					int Size = CheckNumber();
+					if (Size < 0) Size *= -1;
+					std::string* NewSet = new std::string[Size];
+					std::cin.ignore();
+					for (int i = 0; i < Size; i++) {
+						std::cout << "[" << i << "]: ";
+						getline(std::cin, NewSet[i]);
+					}
+					Set<std::string> B(Size, NewSet);
+					A = B;
+				}
+				catch (const char* err)
+				{
+					std::cerr << err;
+				}
+				MenuString(A);
+			}
+			break;
+		case 49:
+			system("cls");
+			MenuSetString(A);
+			break;
+		case 50:
+			system("cls");
+			MenuWorkSetsString(A);
+			break;
+		case 51:
+			system("cls");
+			A.~Set();
+			MenuString(A);
+			break;
+		case 52:
+			system("cls");
+			A.~Set();
+			std::cout << "Thank you! ^_^" << std::endl;
+			exit(0);
+			break;
+		default: std::cout << "Select [0-4]" << std::endl;
+		}
+	}
+}
+
+void MenuWorkSetsDouble(Set<double>&A) {
+	int Repeat = 1;
+	std::cout << A << std::endl;
+	std::cout << "[0]: Sum of sets\n[1]: Difference of sets\n[2]: Union of sets\n[3]: Back\n" << std::endl;
+	while (Repeat != 0)
+	{
+		int k = GetKey();
+		switch (k)
+		{
+		case 48:
+		{
+			system("cls");
+			try
+			{
+				std::cout << "Ented a size: ";
+				int Size = CheckNumber();
+				if (Size < 0) Size *= -1;
+
+				double* NewSet = new double[Size];
+				for (int i = 0; i < Size; i++) {
+					std::cout << "[" << i << "]: ";
+					NewSet[i] = CheckNumberDouble();
+				}
+				Set<double> B(Size, NewSet);
+				A = A + B;
+			}
+			catch (const char* err)
+			{
+				std::cerr << err;
+			}
+			MenuWorkSetsDouble(A);
+			break;
+		}
+		case 49:
+		{
+			system("cls");
+			try
+			{
+				std::cout << "Ented a size: ";
+				int Size = CheckNumber();
+				if (Size < 0) Size *= -1;
+
+				double* NewSet = new double[Size];
+				for (int i = 0; i < Size; i++) {
+					std::cout << "[" << i << "]: ";
+					NewSet[i] = CheckNumberDouble();
+				}
+
+				Set<double> B(Size, NewSet);
+				A = A - B;
+			}
+			catch (const char* err)
+			{
+				std::cerr << err;
+			}
+			MenuWorkSetsDouble(A);
+			break;
+		}
+		case 50:
+		{
+			system("cls");
+			try
+			{
+				std::cout << "Ented a size: ";
+				int Size = CheckNumber();
+				if (Size < 0) Size *= -1;
+				double* NewSet = new double[Size];
+				for (int i = 0; i < Size; i++) {
+					std::cout << "[" << i << "]: ";
+					NewSet[i] = CheckNumberDouble();
+				}
+				Set<double> B(Size, NewSet);
+				A = IntersectionSets(A, B);
+			}
+			catch (const char* err)
+			{
+				std::cerr << err;
+			}
+			MenuWorkSetsDouble(A);
+			break;
+		}
+		case 51:
+			system("cls");
+			MenuDouble(A);
+			break;
+		default: std::cout << "Select [0-3]" << std::endl;
+		}
+	}
+}
+
+
+void MenuSetDouble(Set<double>&A) {
+	int Repeat = 1;
+	std::cout << A << std::endl;
+	std::cout << "[0]: Add a number to a set\n[1]: Remove a number from a set\n[2]: Back\n" << std::endl;
+	while (Repeat != 0)
+	{
+		int k = GetKey();
+		switch (k)
+		{
+		case 48:
+		{
+			system("cls");
+			try
+			{
+				std::cout << "Ented a number: ";
+				double Number = CheckNumberDouble();
+				A = A + Number;
+			}
+			catch (const char* err)
+			{
+				std::cerr << err;
+			}
+			MenuSetDouble(A);
+			break;
+		}
+		case 49:
+			system("cls");
+			try
+			{
+				std::cout << "Ented a number: ";
+				double Number = CheckNumberDouble();
+				A = A - Number;
+			}
+			catch (const char* err)
+			{
+				std::cerr << err;
+			}
+			MenuSetDouble(A);
+			break;
+		case 50:
+			system("cls");
+			MenuDouble(A);
+			break;
+		default: std::cout << "Select [0-2]" << std::endl;
+		}
+	}
+}
+
+void MenuDouble(Set<double>&A) {
+	int Repeat = 1;
+	std::cout << A << std::endl;
+	std::cout << "[0]: Create a new set\n[1]: Edit a set\n[2]: Operations with sets\n[3]: Clear Set\n[4]: Exit the program\n" << std::endl;
+	while (Repeat != 0)
+	{
+		int k = GetKey();
+		switch (k)
+		{
+		case 48:
+			system("cls");
+			{
+				A.~Set();
+				try {
+					std::cout << "Enter a size: ";
+					int Size = CheckNumber();
+					if (Size < 0) Size *= -1;
+					double* NewSet = new double[Size];
+					for (int i = 0; i < Size; i++) {
+						std::cout << "[" << i << "]: ";
+						NewSet[i] = CheckNumberDouble();
+					}
+					Set<double> B(Size, NewSet);
+					A = B;
+				}
+				catch (const char* err)
+				{
+					std::cerr << err;
+				}
+				MenuDouble(A);
+			}
+			break;
+		case 49:
+			system("cls");
+			MenuSetDouble(A);
+			break;
+		case 50:
+			system("cls");
+			MenuWorkSetsDouble(A);
+			break;
+		case 51:
+			system("cls");
+			A.~Set();
+			MenuDouble(A);
+			break;
+		case 52:
+			system("cls");
+			A.~Set();
+			std::cout << "Thank you!^_^" << std::endl;
+			exit(0);
+			break;
+		default: std::cout << "Select [0-4]" << std::endl;
+		}
+	}
+}
+
+void MenuWorkSetsInt(Set<int>&A) {
+	int Repeat = 1;
+	std::cout << A << std::endl;
+	std::cout << "[0]: Sum of sets\n[1]: Difference of sets\n[2]: Union of sets\n[3]: Back\n" << std::endl;
+	while (Repeat != 0)
+	{
+		int k = GetKey();
+		switch (k)
+		{
+		case 48:
+		{
+			system("cls");
+			try
+			{
+				std::cout << "Ented a size: ";
+				int Size = CheckNumber();
+				if (Size < 0) Size *= -1;
+
+				int* NewSet = new int[Size];
+				for (int i = 0; i < Size; i++) {
+					std::cout << "[" << i << "]: ";
+					NewSet[i] = CheckNumber();
+				}
+				Set<int> B(Size, NewSet);
+				A = A + B;
+			}
+			catch (const char* err)
+			{
+				std::cerr << err;
+			}
+			MenuWorkSetsInt(A);
+			break;
+		}
+		case 49:
+		{
+			system("cls");
+			try
+			{
+				std::cout << "Ented a size: ";
+				int Size = CheckNumber();
+				if (Size < 0) Size *= -1;
+
+				int* NewSet = new int[Size];
+				for (int i = 0; i < Size; i++) {
+					std::cout << "[" << i << "]: ";
+					NewSet[i] = CheckNumber();
+				}
+
+				Set<int> B(Size, NewSet);
+				A = A - B;
+			}
+			catch (const char* err)
+			{
+				std::cerr << err;
+			}
+			MenuWorkSetsInt(A);
+			break;
+		}
+		case 50:
+		{
+			system("cls");
+			try
+			{
+				std::cout << "Ented a size: ";
+				int Size = CheckNumber();
+				if (Size < 0) Size *= -1;
+				int* NewSet = new int[Size];
+				for (int i = 0; i < Size; i++) {
+					std::cout << "[" << i << "]: ";
+					NewSet[i] = CheckNumber();
+				}
+				Set<int> B(Size, NewSet);
+				A = IntersectionSets(A, B);
+			}
+			catch (const char* err)
+			{
+				std::cerr << err;
+			}
+			MenuWorkSetsInt(A);
+			break;
+		}
+		case 51:
+			system("cls");
+			MenuInt(A);
+			break;
+		default: std::cout << "Select [0-3]" << std::endl;
+		}
+	}
+}
+
+
+void MenuSetInt(Set<int>&A) {
+	int Repeat = 1;
+	std::cout << A << std::endl;
+	std::cout << "[0]: Add a number to a set\n[1]: Remove a number from a set\n[2]: Back\n" << std::endl;
+	while (Repeat != 0)
+	{
+		int k = GetKey();
+		switch (k)
+		{
+		case 48:
+		{
+			system("cls");
+			try
+			{
+				std::cout << "Ented a number: ";
+				int Number = CheckNumber();
+				A = A + Number;
+			}
+			catch (const char* err)
+			{
+				std::cerr << err;
+			}
+			MenuSetInt(A);
+			break;
+		}
+		case 49:
+			system("cls");
+			try
+			{
+				std::cout << "Ented a number: ";
+				int Number = CheckNumber();
+				A = A - Number;
+			}
+			catch (const char* err)
+			{
+				std::cerr << err;
+			}
+			MenuSetInt(A);
+			break;
+		case 50:
+			system("cls");
+			MenuInt(A);
+			break;
+		default: std::cout << "Select [0-2]" << std::endl;
+		}
+	}
+}
+
+
+void MenuInt(Set<int>&A) {
+	int Repeat = 1;
+	std::cout << A << std::endl;
+	std::cout << "[0]: Create a new set\n[1]: Edit a set\n[2]: Operations with sets\n[3]: Clear Set\n[4]: Exit the program\n" << std::endl;
+	while (Repeat != 0)
+	{
+		int k = GetKey();
+		switch (k)
+		{
+		case 48:
+			system("cls");
+			{
+				A.~Set();
+				try {
+					std::cout << "Enter a size: ";
+					int Size = CheckNumber();
+					if (Size < 0) Size *= -1;
+					int* NewSet = new int[Size];
+					for (int i = 0; i < Size; i++) {
+						std::cout << "[" << i << "]: ";
+						NewSet[i] = CheckNumber();
+					}
+					Set<int> B(Size, NewSet);
+					A = B;
+				}
+				catch (const char* err)
+				{
+					std::cerr << err;
+				}
+				MenuInt(A);
+			}
+			break;
+		case 49:
+			system("cls");
+			MenuSetInt(A);
+			break;
+		case 50:
+			system("cls");
+			MenuWorkSetsInt(A);
+			break;
+		case 51:
+			system("cls");
+			A.~Set();
+			MenuInt(A);
+			break;
+		case 52:
+			system("cls");
+			A.~Set();
+			std::cout << "Thank you! ^_^" << std::endl;
+			exit(0);
+			break;
+		default: std::cout << "Select [0-4]" << std::endl;
+		}
+	}
+}
---
+++ AandSD_lab2/Set.h
@@ -0,0 +1,162 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+template <class T>
+class Set {
+	T *array;
+	int cardinality;
+public:
+	Set() {
+		array = nullptr;
+		cardinality = 0;
+	}
+
+	Set(int cardinality, T*array) {
+		for (int i = 0; i < cardinality; i++) {
+			for (int j = 0; j < cardinality; j++) {
+				if ((array[i] == array[j]) && i != j) throw "[!]: Invalid array format\n";
+			}
+		}
+		this->cardinality = cardinality;
+		this->array = new T[this->cardinality];
+		for (int i = 0; i < this->cardinality; i++) {
+			this->array[i] = array[i];
+		}
+	}
+
+	Set(const Set<T> &rhs) {
+		cardinality = rhs.cardinality;
+		array = new T[cardinality];
+		for (int i = 0; i < cardinality; i++) {
+			array[i] = rhs.array[i];
+		}
+	}
+
+	~Set() {
+		delete[]array;
+		array = 0;
+		cardinality = 0;
+	}
+
+	T operator [] (int index) const{
+		if (index >= cardinality) throw "[!]: Incorrect access by index\n";
+		else return array[index];
+	}
+
+	Set<T> operator = (const Set<T> &rhs) {
+		if (this == &rhs) {
+			return *this;
+		}
+
+		this->~Set();
+		cardinality = rhs.cardinality;
+		array = new T[cardinality];
+		for (int i = 0; i < cardinality; i++) {
+			array[i] = rhs.array[i];
+		}
+		return *this;
+	}
+
+	Set<T> operator + (const Set &rhs) {
+		Set<T> tmp(*this);
+		for (int i = 0; i < rhs.cardinality; i++) {
+			bool flag = true;
+			for (int j = 0; j < cardinality; j++) {
+				if (rhs.array[i] == array[j]) {
+					flag = false;
+					break;
+				}
+			}
+			if (flag == true) {
+				tmp = tmp + rhs.array[i];
+			}
+		}
+		return tmp;
+	}
+
+	Set<T> operator - (const Set<T> &rhs) {
+		Set<T> tmp(*this);
+		for (int i = 0; i < rhs.cardinality; i++) {
+			for (int j = 0; j < cardinality; j++) {
+				if (rhs.array[i] == array[j]) {
+					tmp = tmp - rhs.array[i];
+					break;
+				}
+			}
+		}
+		return tmp;
+	}
+
+	Set<T> operator + (T value) {
+		for (int i = 0; i < cardinality; i++) {
+			if (array[i] == value) throw "[!]: The specified value already belongs to the set\n";
+		}
+		Set<T> tmp;
+		tmp.cardinality = this->cardinality + 1;
+		tmp.array = new T[tmp.cardinality];
+		for (int i = 0; i < tmp.cardinality - 1; i++)
+		{
+			tmp.array[i] = array[i];
+		}
+		tmp.array[tmp.cardinality - 1] = value;
+		return tmp;
+	}
+
+	Set<T> operator - (T value) {
+		int counter = -1;
+		for (int i = 0; i < cardinality; i++) {
+			if (array[i] == value) counter = i;
+		}
+		if (counter != -1) {
+			Set<T> tmp;
+			tmp.cardinality = this->cardinality - 1;
+			tmp.array = new T[tmp.cardinality];
+			if (counter == 0) {
+				for (int i = 0; i < tmp.cardinality; i++)
+				{
+					tmp.array[i] = array[i + 1];
+				}
+			}
+			for (int i = 0; i < counter; i++)
+			{
+				tmp.array[i] = array[i];
+			}
+			for (int i = counter + 1; i < cardinality; i++)
+			{
+				tmp.array[i-1] = array[i];
+			}
+			return tmp;
+		}
+		else throw ("[!]: This element is missing in the set\n");
+	}
+
+	int GetCardinality() const {
+		return cardinality;
+	}
+
+	friend std::ostream& operator << (std::ostream &out, const Set<T> &rhs) {
+		out << "Cardinality is " << rhs.cardinality << "\n";
+		out << "Set = { ";
+		for (int i = 0; i < rhs.cardinality; i++) {
+			out << rhs.array[i] << " ";
+		}
+		out << "}";
+		return out;
+	}
+
+	friend Set<T> IntersectionSets(const Set<T> &rhs1, const Set<T> &rhs2) {
+		Set<T> tmp;
+		for (int i = 0; i < rhs1.cardinality; i++) {
+			for (int j = 0; j < rhs2.cardinality; j++) {
+				if (rhs1.array[i] == rhs2.array[j]) {
+					tmp = tmp + rhs1.array[i];
+					break;
+				}
+			}
+		}
+		return tmp;
+	}
+};
+
+
